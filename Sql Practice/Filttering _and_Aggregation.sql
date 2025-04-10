db92b9f81814e27413f2d1c0719fe854453b2140
SRAVANI KAMJULA <47052243+kamjula@users.noreply.github.com>
2025-04-10 10:00:00 -0400

    Update Filttering _and_Aggregation.sql

--- Sql Practice/Filttering _and_Aggregation.sql
+++ Sql Practice/Filttering _and_Aggregation.sql
@@ -4,3 +4,11 @@ SELECT * FROM students WHERE age > 30;
 
 -- Select IT or Electronics department
 SELECT * FROM students WHERE department IN ('IT', 'Electronics');
+-- Select students with age between 25 and 35
+SELECT * FROM students WHERE age BETWEEN 25 AND 35;
+
+-- Select students not from 'IT' department
+SELECT * FROM students WHERE department != 'IT';
+
+-- Select students from 'Hyderabad' or 'Delhi'
+SELECT * FROM students WHERE city IN ('Hyderabad', 'Delhi');
