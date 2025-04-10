3f55babd3d3fa367db328c72287c4948449ef478
SRAVANI KAMJULA <47052243+kamjula@users.noreply.github.com>
2025-04-10 10:00:00 -0400

    Update Filttering _and_Aggregation.sql
    I wrote SQL queries to find students by age department as part of my Day 2 Data Analyst Preparation for Amazon.
    Practicing filtering logic for real datasets.

--- Sql Practice/Filttering _and_Aggregation.sql
+++ Sql Practice/Filttering _and_Aggregation.sql
@@ -1,1 +1,6 @@
 
+-- Select students older than 30
+SELECT * FROM students WHERE age > 30;
+
+-- Select IT or Electronics department
+SELECT * FROM students WHERE department IN ('IT', 'Electronics');
